520cca33c87c696d8564eed5b43efd1afc9d5a1f
Fabio Luiz Rodrigues <fabioluizrodrigues@gmail.com>
2023-03-25 16:18:17 -0400

    ferat: Adicionado tema claro e provider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path='/pagina-inicial' element={<p><Button>Teste</Button></p>} />
+            <Route path='/pagina-inicial' element={<p><Button variant='contained' color='secondary'>Teste</Button></p>} />
 
             <Route path='*' element={<Navigate to='/pagina-inicial' />} />
         </Routes>
