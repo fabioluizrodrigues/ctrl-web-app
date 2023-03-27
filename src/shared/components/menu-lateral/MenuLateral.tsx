f3f48e8a63b75bb733f058b5a695d7c1830562b7
Fabio Luiz Rodrigues <fabioluizrodrigues@gmail.com>
2023-03-27 18:35:28 -0400

    feat: alternar thema e melhoria na tipografia.

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,7 +1,7 @@
 import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
 import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
-import { useDrawerContext } from '../../contexts';
+import { useAppThemeContext, useDrawerContext } from '../../contexts';
 
 interface IMenuLateralProps {
     children: React.ReactNode
@@ -39,8 +39,9 @@ const ListItemLink : React.FC<IListItemLinkProps> = ({ to, icon, label, onClick 
 export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
 	const theme = useTheme();
 	const smDown = useMediaQuery(theme.breakpoints.down('sm'));
-
 	const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext();
+
+	const { toggleTheme } = useAppThemeContext();
 
 	return (
 		<>
@@ -65,6 +66,16 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
 									onClick={smDown ? toggleDrawerOpen : undefined}
 								/>
 							))}
+						</List>
+					</Box>
+					<Box>
+						<List component='nav'>
+							<ListItemButton onClick={toggleTheme}>
+								<ListItemIcon>
+									<Icon>dark_mode</Icon>
+								</ListItemIcon>
+								<ListItemText primary='Alternar thema'/>
+							</ListItemButton>
 						</List>
 					</Box>
 				</Box>
