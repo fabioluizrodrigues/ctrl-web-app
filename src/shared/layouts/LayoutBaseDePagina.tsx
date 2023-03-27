76070f38793a0fa382098c72eb32601c25c115ab
Fabio Luiz Rodrigues <fabioluizrodrigues@gmail.com>
2023-03-27 18:22:53 -0400

    refactor: Melhorias de responsividade

--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -4,36 +4,38 @@ import { useDrawerContext } from '../contexts';
 interface ILayoutBaseDePaginaProps {
     children: React.ReactNode;
     titulo: string;
+    barraDeFerramentas?: React.ReactNode
 }
 
-export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ children, titulo }) => {
+export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ children, titulo, barraDeFerramentas }) => {
 	const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+	const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
+    
 	const theme = useTheme();
+
 	const {toggleDrawerOpen} = useDrawerContext();
     
 	return (
 		<Box height='100%' display='flex' flexDirection='column' gap={1}>
-			<Box padding={1} display='flex' alignItems='center' height={theme.spacing(12)} gap={1}>
+			<Box padding={1} display='flex' alignItems='center' gap={1} height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)} >
 				{smDown && (
 					<IconButton onClick={toggleDrawerOpen}>
 						<Icon>menu</Icon>
 					</IconButton>
 				)}
-				<Typography variant='h5'>
+				<Typography variant={ smDown ? 'h5' : mdDown ? 'h4' : 'h3'} overflow='hidden' whiteSpace='nowrap' textOverflow='ellipsis'>
 					{titulo}
 				</Typography>
 			</Box>
 
-			<Box>
-				<Typography>
-                    Barra de ferramentas
-				</Typography>
-			</Box>
+			{barraDeFerramentas && (
+				<Box>
+					{barraDeFerramentas}
+				</Box>
+			)}
 
-			<Box>
-				<Typography>
-					{children}
-				</Typography>
+			<Box flex={1} overflow='auto'>
+				{children}
 			</Box>
 		</Box>
 	);
