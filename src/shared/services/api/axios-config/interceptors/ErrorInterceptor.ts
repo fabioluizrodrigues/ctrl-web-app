9c684f9a0f8ca746a256d8fa15d12f48f2c2264e
Fabio Luiz Rodrigues <fabioluizrodrigues@gmail.com>
2023-03-28 14:53:03 -0400

    feat: axios

---
+++ src/shared/services/api/axios-config/interceptors/ErrorInterceptor.ts
@@ -0,0 +1,14 @@
+import { AxiosError } from 'axios';
+
+export const errorInterceptor = (error: AxiosError) => {
+
+	if (error.message === 'Network Error') {
+		return Promise.reject(new Error('Erro de conexão. Parece que você não está conectado.'));
+	}
+
+	if (error.response?.status === 401) {
+		//return Promise.reject(new Error('Erro...'));
+	}
+
+	return Promise.reject(error);
+};
